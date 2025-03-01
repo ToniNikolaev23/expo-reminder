4b254c99c703021ed3846d8764e8c4bf6ae0c730
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-03-01 17:03:42 +0200

    Configure push for ios, eas

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,38 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import galaxies from "@/modules/galaxies";
+import { usePush } from "@/hooks/usePush";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const { registerForPushNotificationsAsync } = usePush();
+  const [expoPushToken, setExpoPushToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log("PUSH", token);
+        if (token) {
+          setExpoPushToken(token);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>{galaxies.hello()}</Text>
       <Text>{galaxies.PI}</Text>
       <Text>{JSON.stringify(galaxies.getDeviceInfo())}</Text>
+      <Text>{expoPushToken}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10,
+  },
+});
